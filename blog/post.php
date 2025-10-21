1d01e7dc0de29d452feb4bc1d300dd9a145bd4eb
Beïza Cosgun <bcosgun781@gmail.com>
2025-10-21 08:37:07 +0200

    Dynamiser le controlleur

--- blog/post.php
+++ blog/post.php
@@ -1,7 +1,12 @@
 <?php
-
-require('src/model.php');
-
-$posts = getPosts();
-
-require('templates/homepage.php');
+ //post.php
+ require('src/model.php');
+ if(isset($_GET['id'])&&$_GET['id']> 0){
+    $identifier =$_GET['id'];
+ }else {
+    echo 'Erreur :aucunidentifiantdebilletenvoyé';
+    die;
+ }
+ $post= getPost($identifier);
+ $comments =getComments($identifier);
+ require('templates/post.php');
